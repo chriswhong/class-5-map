445da8fec36e2d0fa22cf96e2450b4137818d641
Chris Whong <chris.m.whong@gmail.com>
2021-02-25 20:24:15 -0500

    class 4 demo

---
+++ js/scripts.js
@@ -0,0 +1,215 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiY3dob25nIiwiYSI6IjAyYzIwYTJjYTVhMzUxZTVkMzdmYTQ2YzBmMTM0ZDAyIn0.owNd_Qa7Sw2neNJbK6zc1A';
+
+// function to translate NYC land use codes into a color and a description
+var LandUseLookup = (code) => {
+  switch (code) {
+    case 1:
+      return {
+        color: '#f4f455',
+        description: '1 & 2 Family',
+      };
+    case 2:
+      return {
+        color: '#f7d496',
+        description: 'Multifamily Walk-up',
+      };
+    case 3:
+      return {
+        color: '#FF9900',
+        description: 'Multifamily Elevator',
+      };
+    case 4:
+      return {
+        color: '#f7cabf',
+        description: 'Mixed Res. & Commercial',
+      };
+    case 5:
+      return {
+        color: '#ea6661',
+        description: 'Commercial & Office',
+      };
+    case 6:
+      return {
+        color: '#d36ff4',
+        description: 'Industrial & Manufacturing',
+      };
+    case 7:
+      return {
+        color: '#dac0e8',
+        description: 'Transportation & Utility',
+      };
+    case 8:
+      return {
+        color: '#5CA2D1',
+        description: 'Public Facilities & Institutions',
+      };
+    case 9:
+      return {
+        color: '#8ece7c',
+        description: 'Open Space & Outdoor Recreation',
+      };
+    case 10:
+      return {
+        color: '#bab8b6',
+        description: 'Parking Facilities',
+      };
+    case 11:
+      return {
+        color: '#5f5f60',
+        description: 'Vacant Land',
+      };
+    case 12:
+      return {
+        color: '#5f5f60',
+        description: 'Other',
+      };
+    default:
+      return {
+        color: '#5f5f60',
+        description: 'Other',
+      };
+  }
+};
+
+var map = new mapboxgl.Map({
+  container: 'mapContainer', // container ID
+  style: 'mapbox://styles/mapbox/light-v10', // style URL
+  center: [-73.992313,40.678211], // starting position [lng, lat]
+  zoom: 13 // starting zoom
+});
+
+map.on('style.load', function () {
+  // add a geojson source
+  map.addSource('brooklyn-cd6', {
+    type: 'geojson',
+    data: '/data/brooklyn-cd6-truncated.geojson'
+  });
+
+  // add a layer to style and display the source
+  map.addLayer({
+    'id': 'brooklyn-cd6-fill',
+    'type': 'fill',
+    'source': 'brooklyn-cd6',
+    'layout': {},
+    'paint': {
+      'fill-color': {
+        type: 'categorical',
+        property: 'LandUse',
+        stops: [
+          [
+            '01',
+            LandUseLookup(1).color,
+          ],
+          [
+            '02',
+            LandUseLookup(2).color,
+          ],
+          [
+            '03',
+            LandUseLookup(3).color,
+          ],
+          [
+            '04',
+            LandUseLookup(4).color,
+          ],
+          [
+            '05',
+            LandUseLookup(5).color,
+          ],
+          [
+            '06',
+            LandUseLookup(6).color,
+          ],
+          [
+            '07',
+            LandUseLookup(7).color,
+          ],
+          [
+            '08',
+            LandUseLookup(8).color,
+          ],
+          [
+            '09',
+            LandUseLookup(9).color,
+          ],
+          [
+            '10',
+            LandUseLookup(10).color,
+          ],
+          [
+            '11',
+            LandUseLookup(11).color,
+          ],
+
+        ]
+      },
+      'fill-outline-color': '#ccc',
+      'fill-opacity': 0.8
+    }
+  });
+
+  // add an empty data source, which we will use to highlight the lot the user is hovering over
+  map.addSource('highlight-feature', {
+    type: 'geojson',
+    data: {
+      type: 'FeatureCollection',
+      features: []
+    }
+  })
+
+  // add a layer for the highlighted lot
+  map.addLayer({
+    id: 'highlight-line',
+    type: 'line',
+    source: 'highlight-feature',
+    paint: {
+      'line-width': 2,
+      'line-opacity': 0.9,
+      'line-color': 'white',
+    }
+  });
+})
+
+// Create a popup, but don't add it to the map yet.
+var popup = new mapboxgl.Popup({
+  closeButton: false,
+  closeOnClick: false
+});
+
+map.on('mousemove', function (e) {
+  // query for the features under the mouse, but only in the lots layer
+  var features = map.queryRenderedFeatures(e.point, {
+      layers: ['brooklyn-cd6-fill'],
+  });
+
+  if (features.length > 0) {
+    // show the popup
+    // Populate the popup and set its coordinates
+    // based on the feature found.
+
+    var hoveredFeature = features[0]
+    var address = hoveredFeature.properties.Address
+    var landuseDescription = LandUseLookup(parseInt(hoveredFeature.properties.LandUse)).description
+
+    var popupContent = `
+      <div>
+        ${address}<br/>
+        ${landuseDescription}
+      </div>
+    `
+
+    popup.setLngLat(e.lngLat).setHTML(popupContent).addTo(map);
+
+    // set this lot's polygon feature as the data for the highlight source
+    map.getSource('highlight-feature').setData(hoveredFeature.geometry);
+
+    // show the cursor as a pointer
+    map.getCanvas().style.cursor = 'pointer';
+  } else {
+    // remove the Popup
+    popup.remove();
+
+    map.getCanvas().style.cursor = '';
+  }
+
+})
